b2dee19695f849a5c17d7cd3799699e296873b2e
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-23 01:06:28 +0300

    create profile in models

--- awardApp/models.py
+++ awardApp/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+ class Profile(models.Model):
+    dpic = models.ImageField(upload_to = 'images/')
+    bio = models.TextField(max_length=1000)
+    description = models.TextField(max_length=5000)
