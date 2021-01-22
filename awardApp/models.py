cf9e10c7c41a69cdf54ce938d82738936df5361c
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-23 01:16:28 +0300

    create a class instances for projects

--- awardApp/models.py
+++ awardApp/models.py
@@ -8,7 +8,7 @@ from django.db import models
  class Profile(models.Model):
     dpic = models.ImageField(upload_to = 'images/')
     bio = models.TextField(max_length=1000)
-    description = models.TextField(max_length=5000)
+    info = models.TextField(max_length=5000)
 
     def __str__(self):
         return self.username
@@ -19,4 +19,10 @@ from django.db import models
     def delete_profile(self):
         self.delete()
 
-        
+class Projects(models.Model):
+    title = models.CharField(max_length=500)
+    description=models.TextField(max_length=2000)
+    link=models.URLField()
+    image=models.ImageField(upload_to='images/')
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    
