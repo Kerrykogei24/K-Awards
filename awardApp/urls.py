10deaf3abcb2f1f15a31b04edbe1cfe6e80ad4dd
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-24 01:56:02 +0300

    create urls for index

---
+++ awardApp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls import static
+
+
+urlpatterns=[
+    url(r'^$',views.home,name='home'),
+
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
