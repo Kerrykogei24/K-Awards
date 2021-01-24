2b268db0831ef198a4bc7ff4f308dde6b5418420
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-24 10:54:17 +0300

    function that uploads new project

--- awardApp/views.py
+++ awardApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Profile,Projects,Comments,Ratings
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -12,7 +13,20 @@ def index(request):
     return render(request,'index.html',{'all_projects':all_projects})
 
 def profile(request):
-    
+
     all_projects = Projects.objects.filter(user = request.user)
     return render(request,'profile.html',{'all_projects':all_projects})
 
+def new_project(request):
+      if request.method=='POST':
+        form = NewProjectForm(request.POST,request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = request.user
+            project.save()
+
+            return redirect('home')
+
+    else:
+        form = NewProjectForm()
+    return render(request,'new_project.html',{'form':form})
