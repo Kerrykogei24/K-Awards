50c4ca6567553aebc0fa79b6060046088fd5e21c
Kerrykogei24 <kerrykomar.gmail.com>
2021-01-26 20:58:29 +0300

    modified template

--- awardApp/forms.py
+++ awardApp/forms.py
@@ -2,9 +2,6 @@ from django import forms
 from .models import Projects,Profile,Comments
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import NewProjectForm,CommentForm,EditProfileForm
-from django.shortcuts import render,redirect,get_object_or_404
-from django.contrib import messages
 
 class NewProjectForm(forms.ModelForm):
     class Meta:
